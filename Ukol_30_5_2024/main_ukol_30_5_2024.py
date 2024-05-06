90d23968cb02eabd7b4f99794f9a6ee3a01a42f4
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-05-06 21:07:01 +0200

    Task 3

--- Ukol_30_5_2024/main_ukol_30_5_2024.py
+++ Ukol_30_5_2024/main_ukol_30_5_2024.py
@@ -39,3 +39,28 @@ if swapped is not None:
 #Úkol 3
 #Uživatel zadá číslo měsíce (od 1 do 12). Na základě zadaného čísla program zobrazí jedno z následujících: Zima, pokud je číslo 1, 2 nebo 12, Jaro, pokud je číslo v rozsahu od 3 do 5, Léto, pokud je od 6 do 8, Podzim, pokud je od 9 až 11.
 #Pokud je číslo mimo rozsah, zobrazí se chybová zpráva.
+
+def season(num):
+    if num < 1 or num > 12:
+        print("Zadal jsi špatné číslo takový měsíc nemáme ")
+    elif num in [1, 2, 12]:
+        print("Zadal jsi měsíc zima.")
+    elif num >= 3 and num <= 5:
+        print("Zadal jsi měsíc léta.")
+    elif num >= 6 and num <= 8:
+        print("Zadal jsi měsíc léta.")
+    elif num >= 9 and num <= 11:
+        print("Zadal jsi měsíc léta.")
+
+input_season_number = int(input("Zadej číslo měsíce od 1 do 12: "))
+season(input_season_number)
+
+
+
+
+
+
+
+
+
+
