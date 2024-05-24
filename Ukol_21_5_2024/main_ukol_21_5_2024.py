6d9b2bfc21b4abf3c1b575d6698e89d59d55fc6f
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-05-24 15:33:37 +0200

    Task 1 sum

---
+++ Ukol_21_5_2024/main_ukol_21_5_2024.py
@@ -0,0 +1,22 @@
+# Úkol z 21_5_2024
+#Úkol 1
+#Uživatel zadá tři čísla. Program vytiskne součet nebo součin těchto čísel na základě volby uživatele.
+
+#Úkol 2
+#Uživatel zadá tři čísla. Na základě volby uživatele program vytiskne maximálně tři, minimálně tři nebo aritmetický průměr tří čísel.
+
+#Úkol 3
+#Uživatel zadá počet metrů. Na základě volby uživatele program převádí metry na míle, palce nebo yardy.
+
+class Task1:
+    def add(self, num1, num2, num3):
+        return num1+num2+num3
+
+task = Task1()
+a = task.add(2,2,2)
+
+print(a)
+
+
+
+
