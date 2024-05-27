8547cb8d1ee9aab99b501ace606f5b83892ade4a
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-05-27 22:19:41 +0200

    rozšířeny typy testů

--- Ukol_21_5_2024/test_ukol_21_5_2024.py
+++ Ukol_21_5_2024/test_ukol_21_5_2024.py
@@ -9,8 +9,39 @@ class TestSumNum(unittest.TestCase):
         self.assertEqual(self.task.add(-1, 1, 1), 1)
         self.assertEqual(self.task.add(-1, 1, -1), -1)
 
+        # testování s 0
+        self.assertEqual(self.task.add(0,0,0),0)
+        self.assertEqual(self.task.add(0, 1, 2), 3)
+        self.assertEqual(self.task.add(1, 0, 2), 3)
+        self.assertEqual(self.task.add(1, 2, 0), 3)
+
+        #testování s negativními čísly
+        self.assertEqual(self.task.add(-1,-2,-3),-6)
+        self.assertEqual(self.task.add(0,-1,1),0)
+
+        self.assertEqual(self.task.add(-1,0,1),0)
     def test_multiple_numbers(self):
         self.assertEqual(self.task.multiply(1,1,1),1)
+
+        #testování s 0
+        self.assertEqual(self.task.multiply(0,1,2),0)
+        self.assertEqual(self.task.multiply(1, 0, 2), 0)
+        self.assertEqual(self.task.multiply(1, 2, 0), 0)
+
+        #testování s negativními čísly
+        self.assertEqual(self.task.multiply(-1, 2, 3), -6)
+        self.assertEqual(self.task.multiply(-1, -2, 3), 6)
+        self.assertEqual(self.task.multiply(-1,-2,-3),-6)
+
+    def test_not_equal(self):
+        self.assertNotEqual(self.task.add(1,2,3),10)
+        self.assertNotEqual(self.task.multiply(1,2,3),10)
+
+    def test_true_false(self):
+        self.assertTrue(self.task.add(1,2,3) == 6)
+        self.assertFalse(self.task.add(1, 2, 3) == 7)
+        self.assertTrue(self.task.multiply(1, 2, 3) == 6)
+        self.assertFalse(self.task.multiply(1, 2, 3) == 7)
 
 if __name__ == '__main__':
     unittest.main()
